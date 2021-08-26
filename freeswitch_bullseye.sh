ab812aa59522bc44dfab6e1df20b200baf89ee2e
ishavaulakh <85702881+ishavaulakh@users.noreply.github.com>
2021-08-27 02:41:33 +0530

    Update freeswitch_bullseye.sh
    added dependencies

--- freeswitch_bullseye.sh
+++ freeswitch_bullseye.sh
@@ -3,6 +3,7 @@ cd
 echo "Starting Update process"
 apt update -y && apt upgrade -y
 apt-get update && apt-get install -y gnupg2 wget lsb-release
+apt install -y python-dev portaudio19-dev
 echo "Adding sources for freeswitch"
 echo "deb [trusted=yes] http://files.freeswitch.org/repo/deb/debian-release/ buster main" > /etc/apt/sources.list.d/freeswitch.list
 echo "deb-src [trusted=yes] http://files.freeswitch.org/repo/deb/debian-release/ buster main" >> /etc/apt/sources.list.d/freeswitch.list
@@ -16,4 +17,3 @@ systemctl enable freeswitch
 systemctl start freeswitch
 echo "Build complete: System will reboot now."
 echo "Run fs_cli to access switch"
-systemctl reboot
