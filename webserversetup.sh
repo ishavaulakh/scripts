56d40001ae9cc82d25c165bf95686d272ee77758
Ishav Aulakh <85702881+ishavaulakh@users.noreply.github.com>
2022-03-13 03:46:35 +0530

    Update webserversetup.sh

--- webserversetup.sh
+++ webserversetup.sh
@@ -15,11 +15,7 @@ echo Securing MySql
 #Install php
 instphp(){
 echo Installing PHP
-sudo apt install -y php libapache2-mod-php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-imap php-dbase php-libsodium php-mcrypt php-gmp php-symfony-finder php-symfony-proxy-manager-bridge
-  php-symfony-console php-imagick php-twig-doc php-recode php-gd2 php-pragmarx-google2fa-qrcode php-samyoul-u2f-php-server php-mcrypt php-bz2 php-google-recaptcha php-mariadb-mysql-kbs php-phpmyadmin-motranslator php-phpmyadmin-shapefile php-phpmyadmin-sql-parser
-  php-phpseclib php-psr-cache php-psr-container php-psr-log php-symfony-cache php-symfony-cache-contracts php-symfony-config
-  php-symfony-dependency-injection php-symfony-expression-language php-symfony-filesystem php-symfony-service-contracts
-  php-symfony-var-exporter php-symfony-yaml php-tcpdf php-twig php-twig-i18n-extension
+sudo apt install -y php libapache2-mod-php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-imap
 
 }
 
@@ -38,7 +34,6 @@ installnginx()
 {
 apt install -y nginx
 }
-
 # fix permissions
 naturalize()
 {
@@ -47,18 +42,15 @@ find /var/www/html/ -type d -exec chmod 755 {} \;
 find /var/www/html/ -type f -exec chmod 644 {} \;
 }
 
-
 #start installation
 startinstall()
 {
 upgradeos
 disapache
-instphp
 installnginx
+instphp
 instmysql
-naturalize
 mysql_secure_installation
-
+naturalize
 }
 startinstall
-
