920327ec4014f46ab8b726ac526dad8a4578c213
Ishav Aulakh <85702881+ishavaulakh@users.noreply.github.com>
2022-03-17 01:20:12 +0530

    Update webserversetup.sh

--- webserversetup.sh
+++ webserversetup.sh
@@ -20,7 +20,7 @@ mysql_secure_installation
 
 instphp(){
 echo Installing PHP
-sudo apt install -y php php-fpm php-common php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-imap
+sudo apt install -y php php-fpm php-common php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-imap php-imagick php-intl
 }
 
 disable apache2
