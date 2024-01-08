1734d8de207049b135c79bb2670ed853a0ad45ab
Ishav Aulakh <85702881+ishavaulakh@users.noreply.github.com>
2024-01-08 23:22:32 +0530

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -2,4 +2,5 @@ apt update -y && apt upgrade -y
 apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils xrdp -y
 adduser xrdp ssl-cert  
 systemctl restart xrdp
+apt install -y firefox-esr
 ufw allow 3389
