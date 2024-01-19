648f153cf3cc70df6c7f620cc0b7d0d0218fcff0
Ishav Aulakh <85702881+ishavaulakh@users.noreply.github.com>
2024-01-20 03:50:51 +0530

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,4 +1,4 @@
-sudo apt update -y && apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils xrdp -y
 sudo adduser xrdp ssl-cert  
 sudo systemctl restart xrdp
